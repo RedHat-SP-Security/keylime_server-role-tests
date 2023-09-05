3c5f6b80afb8855934016ce3c9596d67602fddd5
Karel Srot <ksrot@redhat.com>
2023-09-05 09:36:22 +0200

    Added /setup/switch_git_branch

---
+++ setup/switch_git_branch/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# vim: dict+=/usr/share/beakerlib/dictionary.vim cpt=.,w,b,u,t,i,k
+. /usr/share/beakerlib/beakerlib.sh || exit 1
+
+DEFAULT=main
+
+rlJournalStart
+
+    rlPhaseStartTest
+        rlAssertRpm git
+        rpm -q redhat-release || rlDie "Not running on RHEL!"
+        rlLogInfo "Current working directory: ${PWD}"
+        rlRun "git status"
+        if [ -z "${SWITCH_TMT_TESTS_BRANCH}" ]; then
+            rlLogInfo "No branch provided using SWITCH_TMT_TESTS_BRANCH variable, using autodetection"
+            VERSION=$(rpm -q --qf "%{VERSION}" redhat-release)
+            rlRun -s "git branch -r"
+            SWITCH_TMT_TESTS_BRANCH=$( grep "origin/rhel-${VERSION}" "$rlRun_LOG" | head -1)
+            [ -z "$SWITCH_TMT_TESTS_BRANCH" ] && SWITCH_TMT_TESTS_BRANCH="$DEFAULT"
+            rlLogInfo "Autodetected branch: $SWITCH_TMT_TESTS_BRANCH"
+        fi
+        rlLogInfo "Switching to branch ${SWITCH_TMT_TESTS_BRANCH}"
+        rlRun "git checkout ${SWITCH_TMT_TESTS_BRANCH}"
+        rlRun "git status"
+    rlPhaseEnd
+
+rlJournalEnd
