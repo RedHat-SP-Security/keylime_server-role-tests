34730e805f43b23059f44164838f679d6bb9718c
Karel Srot <ksrot@redhat.com>
2023-10-12 08:02:40 +0200

    Adjust tests to changes in limeconRunAgent

--- functional/basic-attestation-with-postgresql-db/test.sh
+++ functional/basic-attestation-with-postgresql-db/test.sh
@@ -89,7 +89,7 @@ EOF"
         TESTDIR=`limeCreateTestDir`
         rlRun "limeCreateTestLists"
 
-        rlRun "limeconRunAgent $CONT_AGENT $TAG_AGENT $IP_AGENT $CONT_NETWORK_NAME $PWD/confdir_$CONT_AGENT $TESTDIR"
+        rlRun "limeconRunAgent $CONT_AGENT $TAG_AGENT $IP_AGENT $CONT_NETWORK_NAME $TESTDIR keylime_agent $PWD/confdir_$CONT_AGENT $PWD/cv_ca"
         rlRun "limeWaitForAgentRegistration ${AGENT_ID}"
 
     rlPhaseEnd
